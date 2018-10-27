b4dadfe15a3f7638a4078297fa09e81993a58679
David Noh <davidnohj@gmail.com>
2018-10-27 13:50:58 -0700

    re

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{HttpModule} from '@angular/http';
+import{FormsModule} from '@angular/forms'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingComponent } from './components/shopping/shopping.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import {ServiceService} from './service/service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
